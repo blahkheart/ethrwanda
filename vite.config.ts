25cedf7e00e80bb82d181a9588590879bf12f501
blahkheart <dannithomx@gmail.com>
2025-02-22 02:17:15 +0200

    fix: hot reload bug in containerized environmnent fixed

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,13 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true, // Allows access from the host machine
+    port: 5173, // Adjust to your desired port
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
